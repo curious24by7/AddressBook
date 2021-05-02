2ebad1fde5e86812153db417343cdf533e804c72
sohamdhamnaskar <sohamdham123@gmail.com>
2021-05-02 14:35:52 +0530

    [soham]Changes: Representation of saved Contacts.

--- AddressBook/AddressBook.cs
+++ AddressBook/AddressBook.cs
@@ -18,7 +18,9 @@ namespace AddressBook
         {
             foreach (var contact in pcontacts)
             {
-                Console.WriteLine($"SAVED CONTACT IS : {contact.FirstName}, {contact.LastName}, {contact.Address}, {contact.City}, {contact.State}, {contact.ZipCode}, {contact.PhoneNumber}, {contact.Email}");
+                Console.WriteLine("SAVED CONTACT IS :\n"+contact.FirstName +contact.LastName+ "\nAddress: " +contact.Address+ "\nCity: " +contact.City+ "\nState: " +contact.State+"\nZip Code: " +contact.ZipCode+ "\nPhone Number: " +contact.PhoneNumber+ "\nEmail: " +contact.Email);
+                //to represent info in 1 line 
+                //{contact.LastName}, {contact.Address}, {contact.City}, {contact.State}, {contact.ZipCode}, {contact.PhoneNumber}, {contact.Email}
             }
         }
     }
