1b6267819318a2749b6eb6c355e90470b47ded0e
sohamdhamnaskar <sohamdham123@gmail.com>
2021-05-19 21:36:00 +0530

    [soham]Added:ExceptionHandling in FileIo

--- AddressBook/FileIO.cs
+++ AddressBook/FileIO.cs
@@ -12,14 +12,18 @@ namespace AddressBook
         public static void WriteDataToTxt(Dictionary<string,Contact> DictName)
         {
             const string PATH = @"C:\Users\soham\source\repos\AddressBook\AddressBook\StoredContact.txt";
-            using (TextWriter tw = new StreamWriter(PATH))
+            if (File.Exists(PATH))
             {
-                tw.WriteLine(string.Format("FirstName,LastName,Address,City,State,ZipCode,PhoneNumber,Email"));
-                foreach(var element in DictName)
+                using (TextWriter tw = new StreamWriter(PATH))
                 {
-                    tw.WriteLine(string.Format($"{element.Value.FirstName},{element.Value.LastName},{element.Value.Address},{element.Value.City},{element.Value.State},{element.Value.ZipCode},{element.Value.PhoneNumber},{element.Value.Email}"));
+                    tw.WriteLine(string.Format("FirstName,LastName,Address,City,State,ZipCode,PhoneNumber,Email"));
+                    foreach (var element in DictName)
+                    {
+                        tw.WriteLine(string.Format($"{element.Value.FirstName},{element.Value.LastName},{element.Value.Address},{element.Value.City},{element.Value.State},{element.Value.ZipCode},{element.Value.PhoneNumber},{element.Value.Email}"));
+                    }
                 }
             }
+            else Console.WriteLine("File doesn't exist, Check the Path");
         }
     }
 }
